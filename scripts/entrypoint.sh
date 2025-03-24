7c3a14cbf9cef0995a964edd46225c71130a3d70
Marcos Serra <marcosserra133@gmail.com>
2025-03-24 17:27:39 -0300

    build(entrypoint): ⚙️ adiciona script de entrypoint para inicialização do container
    - Adiciona verificações de comandos com tratamento de erros
    - Configura migrações do Django e arquivos estáticos
    - Implementa criação automática de superusuário
    - Configura inicialização do Gunicorn

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Função para verificar falhas em comandos
+check_command() {
+    if [ $? -ne 0 ]; then
+        echo "Erro na execução do comando: $1"
+        exit 1
+    fi
+}
+
+# Criar migrações
+echo "🟡 Criando migrações..."
+python manage.py makemigrations
+check_command "makemigrations"
+
+# Rodar migrações
+echo "🟡 Rodando migrações..."
+python manage.py migrate
+check_command "migrações"
+
+# Coletar Arquivos Staticos
+echo "🟡 Coletando arquivos estaticos..."
+python manage.py collectstatic --noinput
+
+# Verificar e criar superusuário se necessário
+echo "🟡 Verificando e criando superusuário se necessário..."
+if [ -z "${DJANGO_SUPERUSER_USERNAME}" ] || [ -z "${DJANGO_SUPERUSER_EMAIL}" ] || [ -z "${DJANGO_SUPERUSER_PASSWORD}" ]; then
+    echo "Erro: variáveis de ambiente do superusuário não estão definidas."
+    exit 1
+fi
+
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
+    User.objects.create_superuser(
+        "${DJANGO_SUPERUSER_USERNAME}",
+        "${DJANGO_SUPERUSER_EMAIL}",
+        "${DJANGO_SUPERUSER_PASSWORD}"
+    )
+EOF
+check_command "criação de superusuário"
+echo "🟢 Superuser criado com sucesso"
+
+# Iniciando o servidor gunicorn
+echo "🟢 Iniciando servidor Gunicorn..."
+gunicorn core.wsgi:application --bind 0.0.0.0:8000
+check_command "início do servidor Gunicorn"
+
+# Iniciar o servidor
+# echo "🟢 Iniciando servidor Django..."
+# python manage.py runserver 0.0.0.0:8000
+# check_command "início do servidor Django"
