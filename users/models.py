92741bced669a6300363c5af1752ea647afb9b47
Marcos Serra <marcosserra133@gmail.com>
2025-03-26 18:57:25 -0300

    feat(users): ✨ adiciona modelo de usuário personalizado com gerenciador

--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O email é obrigatório')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, max_length=255, verbose_name='Email')
+    name = models.CharField(max_length=150, blank=True, verbose_name='Nome')
+    is_active = models.BooleanField(default=True, verbose_name='Ativo')
+    is_staff = models.BooleanField(default=False, verbose_name='Equipe')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Editado em')
+    groups = models.ManyToManyField('auth.Group', blank=True, related_name='custom_user_set', related_query_name='custom_user', verbose_name='Grupos')
+    user_permissions = models.ManyToManyField('auth.Permission', blank=True, related_name='custom_user_set', related_query_name='custom_user', verbose_name='Permissões do usuário')
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
