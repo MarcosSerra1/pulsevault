df483a09c34ac2d4f0de4ec19536a4651ec26b48
Marcos Serra <marcosserra133@gmail.com>
2025-03-27 01:59:00 -0300

    refactor(serializer): ♻️ simplifica CustomUserSerializer
    - Implementa create usando create_user do manager
    - Adiciona tratamento especial para senha no update
    - Define campos write_only para senha
    - Configura campos read_only para datas

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,19 @@ from .models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
 
     class Meta:
         model = CustomUser
-        fields = ['id', 'email', 'name', 'is_active', 'created_at', 'updated_at', 'groups']
+        fields = ('id', 'email', 'password', 'name', 'is_active',
+                  'is_staff', 'created_at', 'updated_at')
+        read_only_fields = ('created_at', 'updated_at')
+
+    def create(self, validated_data):
+        return CustomUser.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+        return super().update(instance, validated_data)
