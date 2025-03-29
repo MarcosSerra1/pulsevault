42e0fba224cec69286a3df9b6a492f2efd0314f8
Marcos Serra <marcosserra133@gmail.com>
2025-03-28 22:50:11 -0300

    refactor(core): ♻️ atualiza caminho do CurrentUserMiddleware

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'common.middleware.CurrentUserMiddleware',
+    'core.middleware.CurrentUserMiddleware',
 ]
 
 ROOT_URLCONF = "core.urls"
